1b848c010269b1acbe248bf3d7ed6b8907f167c8
maridilo <146822054+maridilo@users.noreply.github.com>
2023-11-30 11:45:42 +0100

    commit

--- main.cpp
+++ main.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+#include<string>
+#include<cstdio>
 
+using namespace std;
+
+struct ColorConsole
+{
+    static constexpr auto fg_blue = "\033[34m";
+    static constexpr auto bg_white = "\033[47m";
+};
+
+struct ConsoleBox
+{
+    void new_text() {/*...*/}
+    void set_text
+};
